0a04eb2f84bc2fc69166bb34261ae5789c986d34
jBuran <burankurmanaliev4@mail.ru>
2022-09-10 21:00:10 +0600

    цикл

--- index.js
+++ index.js
@@ -16,13 +16,21 @@ for (let i = 0; i < 2; i++) {
    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
       personalMovieDB.movies[a] = b;
       console.log('done')
-    }
-    else{
+   }
+   else {
       console.log('error');
       i--;
-    }
- 
-}
+   }
 
+}
+if (personalMovieDB.count < 10) {
+   console.log('few movie u watch');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+   console.log('good');
+} else if (personalMovieDB.count >= 30) {
+   console.log('cool');
+} else {
+   console.log('error');
+}
 
 console.log(personalMovieDB)
