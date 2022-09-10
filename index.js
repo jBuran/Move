42e939cfdcf9433c78981203af6a20da6b883c99
jBuran <burankurmanaliev4@mail.ru>
2022-09-10 20:59:56 +0600

    js цикл

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
    const a = prompt("Your last watched films?", ""),
       b = prompt("Rate this film:", "");
-
-   personalMovieDB.movies[a] = b;
+   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done')
+    }
+    else{
+      console.log('error');
+      i--;
+    }
+ 
 }
 
 
