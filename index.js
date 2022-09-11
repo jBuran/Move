1cad9fdceb2282ec27754cf601816ca54d9b3059
jBuran <burankurmanaliev4@mail.ru>
2022-09-12 02:25:55 +0600

    обернул все в функцию

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 
-const numberOfFlilms = +prompt("How many films did u watch?", "")
+let numberOfFlilms;
+function start() {
+   numberOfFlilms = +prompt("How many films did u watch?", "")
+
+   while (numberOfFlilms == '' || numberOfFlilms == null || isNaN(numberOfFlilms)) {
+      numberOfFlilms = +prompt("How many films did u watch?", "")
+   }
+}
+start()
 
 const personalMovieDB = {
    count: numberOfFlilms,
@@ -10,27 +18,35 @@ const personalMovieDB = {
    privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-   const a = prompt("Your last watched films?", ""),
-      b = prompt("Rate this film:", "");
-   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-      console.log('done')
+function rememberMyfilms() {
+   for (let i = 0; i < 2; i++) {
+      const a = prompt("Your last watched films?", ""),
+         b = prompt("Rate this film:", "");
+      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b;
+         console.log('done')
+      }
+      else {
+         console.log('error');
+         i--;
+      }
    }
-   else {
-      console.log('error');
-      i--;
-   }
+}
 
+rememberMyfilms()
+
+function detectPersonalLevel() {
+   if (personalMovieDB.count < 10) {
+      console.log('few movie u watch');
+   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log('good');
+   } else if (personalMovieDB.count >= 30) {
+      console.log('cool');
+   } else {
+      console.log('error');
+   }
 }
-if (personalMovieDB.count < 10) {
-   console.log('few movie u watch');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-   console.log('good');
-} else if (personalMovieDB.count >= 30) {
-   console.log('cool');
-} else {
-   console.log('error');
-}
+
+detectPersonalLevel()
 
 console.log(personalMovieDB)
